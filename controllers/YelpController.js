badb7478082095365282cf7d391c907c5ea94175
Chad Rutherford <c.ruth544@gmail.com>
2016-03-26 16:18:00 -0700

    now doing yelp search

--- controllers/YelpController.js
+++ controllers/YelpController.js
@@ -9,29 +9,28 @@ module.exports = {
       token: process.env.YELP_TOKEN_KEY,
       token_secret: process.env.YELP_TOKEN_SECRET
     })
-    console.log("Req: ", req.body)
     var body = JSON.parse(Object.keys(req.body)[0])
-    console.log("Body: ", body)
     var searchParams = {
-      term: req.body.term,
-      category: req.body.category,
-      location: req.body.location,
-      limit: req.body.limit,
+      term: body.term,
+      category: body.category,
+      location: body.location,
+      limit: body.limit,
       offset: '0',
       sort: '0'
     }
 
     console.log("Search params: ", searchParams)
 
-    // yelp.search(searchParams)
-    //   .then(function (data) {
-    //     var ventures = {}
+    yelp.search(searchParams)
+      .then(function (data) {
+        var ventures = {}
+        console.log("Yelp Data: ", data)
 
 
-    //     res.json(ventures)
-    //   }, function (err) {
-    //     console.log(err)
-    //   })
-    res.json({success: true})
+        res.json(ventures)
+      }, function (err) {
+        console.log(err)
+      })
+    // res.json({success: true})
   }
 }
